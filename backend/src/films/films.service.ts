ac2719f60441bb093a2eb13330e7419e98d5d613
fenyadim <dimos9813@gmail.com>
2025-05-04 14:18:10 +0800

    feat: Реализации сущности films

---
+++ backend/src/films/films.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+import { Film } from './schemas/Film.schema'
+
+@Injectable()
+export class FilmsService {
+  constructor(@InjectModel(Film.name) private filmsModel: Model<Film>) {}
+
+  async getFilms() {
+    const films = await this.filmsModel.find().select('-schedule -_id')
+
+    return {
+      total: 0,
+      items: films,
+    }
+  }
+
+  async getOneFilm(id: string) {
+    const film = await this.filmsModel.findOne({ id })
+
+    return {
+      total: 0,
+      items: film.schedule,
+    }
+  }
+}
